87d7d85d4b04f82006443a93ffd9e09c54522bd4
lilinll77 <lilinll77@gmail.com>
2017-09-11 15:56:59 +0800

    instrument demo craate
    instrument demo 代码创建

---
+++ LLInstrumentDemo/LLInstrumentDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LLInstrumentDemo
+//
+//  Created by lilinli on 2017/9/11.
+//  Copyright © 2017年 lilinli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
